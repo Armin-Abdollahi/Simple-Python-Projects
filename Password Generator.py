94684c009cb79ec8cef9d90aa768b6ad93ab5c4c
Armin Abdollahi <Armin.Abdollahi.64@gmail.com>
2023-08-09 00:08:53 +0330

    Add files via upload

--- Password Generator.py
+++ Password Generator.py
@@ -4,5 +4,16 @@
 # In[ ]:
 
 
+import random
+import string
 
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    
+    password = ''.join(random.choice(characters)for _ in range(length))
+    
+    return password
+
+password = generate_password(10)
+print(password)
 
