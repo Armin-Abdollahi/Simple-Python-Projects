9ae4f0f79bd0447f02436724732e40ca9d9def08
Armin Abdollahi <Armin.Abdollahi.64@gmail.com>
2023-08-09 00:09:07 +0330

    Add files via upload

--- QR Code Generator.py
+++ QR Code Generator.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 
+pip install qrcode
 
+
+# In[2]:
+
+
+import qrcode
+
+data = "https://github.com/Armin-Abdollahi"
+
+qr = qrcode.QRCode(version=1, error_correction = qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
+
+qr.add_data(data)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color = "White")
+
+img.show(img)
+img.save("qrcode.png")
 
