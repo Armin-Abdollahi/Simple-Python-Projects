a62e439c336ad902c7515a56b916c697d6ee690f
Armin Abdollahi <Armin.Abdollahi.64@gmail.com>
2023-08-10 15:41:29 +0330

    Add files via upload

--- Email Slicer.py
+++ Email Slicer.py
@@ -4,5 +4,16 @@
 # In[ ]:
 
 
+email = input("Enter your email address: ")
 
+# Find the index of the "@" symbol
+at_index = email.index("@")
+
+# Slice the username and domain name from the email address
+username = email[:at_index]
+domain = email[at_index+1:]
+
+# Print the username and domain name
+print("Username:", username)
+print("Domain:", domain)
 
