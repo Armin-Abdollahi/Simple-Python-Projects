973995877d6604431b62fcfc04e78f7768f95e6b
Armin Abdollahi <Armin.Abdollahi.64@gmail.com>
2023-08-11 02:46:26 +0330

    Add files via upload

--- Youtube Downloader.py
+++ Youtube Downloader.py
@@ -4,5 +4,30 @@
 # In[ ]:
 
 
+pip install pytube
 
+
+# In[ ]:
+
+
+from pytube import YouTube
+
+# Enter the video link
+video_url = input("Enter the YouTube video URL: ")
+
+# Create YouTube object using video link
+yt = YouTube(video_url)
+
+# Show video details
+print("Video Title:", yt.title)
+print("Video Length:", yt.length, "seconds")
+print("Video Views:", yt.views)
+
+# Choose the download format
+stream = yt.streams.get_highest_resolution()
+
+# video download
+print("Downloading...")
+stream.download()
+print("Download completed!")
 
