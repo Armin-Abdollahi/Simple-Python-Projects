054caa5a554c04c4f35abc4149241c2f2f3b94b2
Armin Abdollahi <Armin.Abdollahi.64@gmail.com>
2023-08-11 02:41:19 +0330

    Add files via upload

--- Dice Simulation.py
+++ Dice Simulation.py
@@ -4,5 +4,22 @@
 # In[ ]:
 
 
+import random
 
+def roll_dice():
+    return random.randint(1,6)
+
+def simulate_dice(num_rolls):
+    results = []
+    for _ in range(num_rolls):
+        result = roll_dice()
+        results.append(result)
+    return results
+
+num_rolls = int(input("Enter the number of dice rolls: "))
+dice_results = simulate_dice(num_rolls)
+
+print("The results of the dice roll:")
+for result in dice_results:
+    print(result)
 
